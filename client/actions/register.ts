bf3c8bb79cb3402105bed4d6e852391374315483
Not-Sarthak <notsarthakshah@gmail.com>
2024-06-16 01:15:44 +0530

    feat: email verification using resend

--- client/actions/register.ts
+++ client/actions/register.ts
@@ -2,9 +2,11 @@
 
 import * as z from "zod";
 import bcrypt from "bcrypt";
-import { db } from "@/lib/db"
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -17,18 +19,20 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const existingUser = await getUserByEmail(email);
-  
+
   if (existingUser) {
     return { error: "Email already in use!" };
   }
 
   await db.user.create({
     data: {
-        name,
-        email,
-        password: hashedPassword,
-    }
-  })
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
 
+  const verificationToken = await generateVerificationToken(email);
+  await sendVerificationEmail(verificationToken.email, verificationToken.token);
   return { success: "User Created!" };
 };
--- client/components/auth/register-form.tsx
+++ client/components/auth/register-form.tsx
@@ -59,6 +59,23 @@ export const RegisterForm = () => {
           <div className="space-y-4">
             <FormField
               control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="Sarthak"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
               name="email"
               render={({ field }) => (
                 <FormItem>
@@ -87,23 +104,6 @@ export const RegisterForm = () => {
                       disabled={isPending}
                       placeholder="******"
                       type="password"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                      placeholder="Sarthak"
                     />
                   </FormControl>
                   <FormMessage />
