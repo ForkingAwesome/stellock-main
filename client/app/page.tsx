ae47670ad9919514eb79c9003b3afcf8a7109397
Not-Sarthak <notsarthakshah@gmail.com>
2024-06-15 14:28:08 +0530

    add:card

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,32 @@
 import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
   return (
-    <Button size="lg">Click</Button>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-yellow-400 to-yellow-800">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+        <LoginButton>
+          <Button variant="secondary" size="lg">
+            Sign In
+          </Button>
+        </LoginButton>
+      </div>
+    </main>
   );
 }
